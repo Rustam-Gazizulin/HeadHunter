929e94915353dfa9da351afe4239044ef0a136ef
rgazizulin174 <rgazizulin174@gmail.com>
2023-01-12 20:03:41 +0500

    add model company, viewImage

--- HeadHunter/urls.py
+++ HeadHunter/urls.py
@@ -1,11 +1,16 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from HeadHunter import settings
 from vacancies import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
     path('vacancy/', include('vacancies.urls')),
+    path('company/', include('companies.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
