84672141209440beadee896dfc802b99e8c21fdf
rgazizulin174 <rgazizulin174@gmail.com>
2023-01-22 23:36:58 +0500

    add pagination

--- HeadHunter/settings.py
+++ HeadHunter/settings.py
@@ -133,4 +133,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-TOTAL_ON_PAGE = 3
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
