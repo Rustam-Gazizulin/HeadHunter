da61e727a2052d4c7140db01d787ad5dce6eed12
rgazizulin174 <rgazizulin174@gmail.com>
2023-01-27 14:16:16 +0500

    создал абстрактного юзера с дополнительным полем sex

--- HeadHunter/settings.py
+++ HeadHunter/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "authentication",
     "vacancies",
     "companies",
 ]
@@ -139,3 +140,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+AUTH_USER_MODEL = 'authentication.User'
